4f3abe402686eb5a2ada9c6cc0fdf8145bef2189
Andhieka Putra <andhieka.putra@gmail.com>
2015-10-19 22:22:15 +0800

    Initial commit

---
+++ CS3219-CoreImage.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import XCPlayground
+
+var str = "Hello, playground"
+var context = CIContext()
+
+var uiImage = UIImage(named: "crowd.jpg")
+var originalImage = CIImage(CGImage: uiImage!.CGImage!)
+var imageView = UIImageView()
+
+func increaseBrightness(image: CIImage, amount: Double) -> UIImage {
+    var result = image
+    // enter your core image magic here
+    
+    let brightnessFilter = CIFilter(name: "CIColorControls")!
+    brightnessFilter.setValue(image, forKey: kCIInputImageKey)
+    brightnessFilter.setValue(amount, forKey: kCIInputBrightnessKey)
+    result = brightnessFilter.outputImage!
+    
+    let renderedImage = context.createCGImage(result, fromRect: image.extent)
+    return UIImage(CGImage: renderedImage)
+}
+
+func chainFilters(image: CIImage, amount: Double) -> UIImage {
+    var result = image
+    
+    // demo your CIFilter here
+    let sepiaFilter = CIFilter(name: "CISepiaTone")
+    sepiaFilter?.setValue(image, forKey: kCIInputImageKey)
+    
+    let vignetteFilter = CIFilter(name: "CIVignette")
+    vignetteFilter?.setValue(sepiaFilter?.outputImage, forKey: kCIInputImageKey)
+    vignetteFilter?.setValue(amount, forKey: kCIInputIntensityKey)
+    result = vignetteFilter!.outputImage!
+    
+    let renderedImage = context.createCGImage(result, fromRect: image.extent)
+    return UIImage(CGImage: renderedImage)
+}
+
+// demo adjusting brightness
+for var i = -0.75; i < 1.0; i = i + 0.25 {
+    let image = increaseBrightness(originalImage, amount: i)
+    imageView.image = image
+    XCPShowView("Brightness", view: imageView)
+}
+
+// demo old effect
+for var i: Double = 0; i < 1.0; i = i + 0.2 {
+    
+}
+
